b0c4bed65ad5d20b41c54117ea5432c45275acfb
xdminsy <xsy2293419066@gmail.com>
2019-01-29 14:54:17 +0800

    add vim visual highlight

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 ln .ycm_extra_conf.py ~
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim ~/.vim/bundle/Vundle.vim
+mv ~/.emacs.d ~/.emacs.d.backup
+ln .emacs.d ~
 mv ~/.vimrc ~/.vimrc.local    # Backup your own .vimrc and, it will also be executed.
 ln .vimrc ~
 vim +PluginInstall +qall
 ln .shrc ~
-echo "source ~/.shrc" >> ~/.$(basename $SHELL)rc
